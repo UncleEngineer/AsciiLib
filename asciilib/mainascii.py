4f98eb022e46e0053585aa60d6ff76814e7cd38b
Uncle Engineer <loong.wissawakorn@gmail.com>
2021-04-22 16:06:41 +0700

    Update Class Name and add example

--- asciilib/mainascii.py
+++ asciilib/mainascii.py
@@ -1,1 +1,29 @@
-mainascii.py
+# mainascii.py
+
+############FIND ASCII ART IN https://www.asciiart.eu/ ##############
+class UncleEngineer:
+
+	def __init__(self):
+		self.name = 'Uncle'
+		self.lastname = 'Engineer'
+
+	def myart(self):
+		art = '''
+------------------------------------------
+		                       .-'~~~-.
+                     .'o  oOOOo`.
+                    :~~~-.oOo   o`.
+    Uncle Enginer    `. \ ~-.  oOOo.
+                       `.; / ~.  OO:
+                       .'  ;-- `.o.'
+                      ,'  ; ~~--'~
+                      ;  ;
+_______\|/__________\\;_\\//___\|/________
+------------------------------------------
+		'''
+		print(art)
+
+
+if __name__ == '__main__':
+	test = UncleEngineer()
+	test.myart()
